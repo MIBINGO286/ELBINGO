236c245905eb73ba67c2198fc02d190751873acd
MIBINGO286 <tokiovalencia20@gmail.com>
2025-06-29 16:22:56 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbwLf2p_I86ZAg_u3hQpkkcDKQ0zI4r480EwCmmA-CaLSLV7OAyjZVtqRJ_w2XGGRxylYw/exec",
-  SECRET: "Jrr035$$*",
+  SECRET: "1234",
   ADMIN_PASS_HASH: "e3cfbe83ca38063b52035cf88836b5c8" // MD5 de Jrr035$$*
 };
